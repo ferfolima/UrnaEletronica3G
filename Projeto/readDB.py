f9307930cbf824db963bee2795469c16b6ec450b
Fernando Teodoro de Lima <ferfo.lima@gmail.com>
2016-06-30 20:28:56 -0300

    Removendo arquivos nao utilizados

--- Projeto/readDB.py
+++
@@ -1,8 +0,0 @@
-import h5py
-
-f = h5py.File('ex_table_09.h5', 'r')
-
-g = f['table1']
-
-for i in g:
-	print i
--- Projeto/readString.py
+++
@@ -1,9 +0,0 @@
-filein = open('string.txt', 'r')
-string = filein.readline()
-filein.close()
-
-print string
-
-fileout = open('string.txt', 'aw')
-fileout.write('\n\n\n\n\nIsso eh um teste')
-fileout.close()
